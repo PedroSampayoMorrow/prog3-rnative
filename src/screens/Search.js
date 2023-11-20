3640972337bc5391a975f4ae2878c086c7e4e801
T0M4SCG <tconchagiovana@udesa.edu.ar>
2023-11-20 05:57:07 -0300

    Pila de cosas

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,62 +1,69 @@
 import React, { Component } from 'react';
-import { Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet, Image, FlatList } from 'react-native';
 import Buscador from '../components/Buscador'
 import { db } from '../firebase/config'
 import { View } from 'react-native';
+import UsuarioBuscador from '../components/UsuarioBuscador';
 
 class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
             pBuscada:'',
-            users:[]
+            users:[],
+            usersBuscados:[],
+            err:""
     }
+    }
+    componentDidMount(){
+        db.collection('usuarios')
+        .onSnapshot(
+          users => {
+              let usuarios = []
+              users.forEach(
+                  user => {
+                      let info = user.data()
+                      usuarios.push({
+                          username: info.username,
+                          fotoperfil: info.fotoPerfil,
+                          email: info.owner
+                      })
+                  }
+                  )
+                  this.setState({users:usuarios})
+              })
     }
     palabraBuscada(texto){
         this.setState({pBuscada:texto},()=> console.log(this.state.pBuscada))
     }
     buscar(){
-        db.collection('usuarios')
-          .onSnapshot(
-            users => {
-                let usuarios = []
-                let usuariosBuscados = []
-                users.forEach(
-                    user => {
-                        let info = user.data()
-                        usuarios.push({
-                            username: info.username,
-                            fotoperfil: info.fotoPerfil,
-                            email: info.owner
-                        })
-                    }
-                )
-            usuarios.filter(user=>{
+        let usuariosBuscados = []
+            this.state.users.forEach(user=>{
                 if (user.username.toLowerCase().includes(this.state.pBuscada.toLowerCase()) || user.email.toLowerCase().includes(this.state.pBuscada.toLowerCase())) {
                     usuariosBuscados.push(user)
                 }
             })
-            this.setState({users:usuariosBuscados},()=> console.log(this.state.users))
+            if(usuariosBuscados.toString() == []){
+                this.setState({err: "No hay resultados"})
             }
-          )
-    }
+            else{
+                this.setState({usersBuscados:usuariosBuscados, err: ""})
+            }
+            }
+    
     render() { 
         return ( 
         <View style={styles.contenedor}>
-        
-        <View>
-        
+        <View style={styles.input}>
         <Buscador style={styles.buscador} palabraBuscada={texto => this.palabraBuscada(texto)}/>
         <TouchableOpacity style={styles.button} onPress={()=> this.buscar()}><Text style={styles.texto}>Buscar</Text></TouchableOpacity>
         
         </View>
-        {this.state.users.forEach(user =>{
-            <View>
-            {user.fotoperfil.includes('https://firebasestorage') ? <Image source={{uri:user.fotoPerfil}} resizeMode='contains' style={styles.img} /> : <Image source={require('../../assets/FotoPerfilDefault.jpg')} resizeMode='contains' style={styles.img}/>}
-            <Text style={styles.textoUsuario}>{user.username}</Text>
-            <Text style={styles.textoUsuario}>{user.email}</Text>
-            </View>
-        })}
+
+        {this.state.err == "" ?(this.state.usersBuscados.toString() == [] ? <Text>Aqui aparecera lo que busque</Text>
+        :
+            <FlatList style={styles.flatList} data={this.state.usersBuscados} keyExtractor={item => item.email} renderItem={({item})=> <UsuarioBuscador data={item}/>}/>
+        ): <Text>{this.state.err}</Text>}
         </View>
         );
     }
@@ -64,7 +71,7 @@ class Search extends Component {
 const styles = StyleSheet.create({
     button: {
         backgroundColor: 'blue',
-        width:'60%',
+        width:'100%',
         padding: 10,
         borderRadius: 5,
         marginTop:5
@@ -77,11 +84,10 @@ const styles = StyleSheet.create({
         fontSize: 10
       },
       contenedor:{
-        height: 'max-content',
-        width: '100%',
+        flex: 1,
+        height: '100%',
         display: 'flex',
         alignItems: 'center',
-        justifyContent:'center',
         paddingTop:20
       },
       buscador:{
@@ -91,6 +97,17 @@ const styles = StyleSheet.create({
         height:150,
         width:150,
         borderRadius: `50%`
+    },
+    usuarios:{
+        padding: 10,
+        height:"max-content"
+    },
+    flatList:{
+        width:"100%"
+    },
+    input:{
+        width:"80%"
     }
+
 })
 export default Search;
